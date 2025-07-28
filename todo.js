48862521ea6519a58c8bb77700b961975ad51d26
yoodeve <janhold@naver.com>
2025-07-29 08:08:28 +0900

    feat: add기능 1차완성

--- todo.js
+++ todo.js
@@ -1,19 +1,42 @@
 'use strict';
 
 const todoKey = "todos";
-const todoList = [];
+const todoList=[];
+const todoListlocalSTRGArray = JSON.parse(localStorage.getItem(todoKey));
+
+const todoMainInput = document.querySelector("#todo-input");
+const deleteBtn = document.querySelector("button.delete-todo");
+const todoDOM = document.querySelector("#todo-item");
+const todoContainer = document.querySelector("#todo-list ul");
+let todoId = 0;
+
+function renderTodo() {
+  todoContainer.innerHTML = "";
+  todoList.forEach(e => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <input type="checkbox" id="todo-${e.todoId}">
+      <label for="todo-${e.todoId}">${e.text}</label>
+      <button class="delete-todo" data-id="${e.todoId}">Delete</button>
+    `;
+    todoContainer.appendChild(li);
+  });
+}
+
 
 function addTodo() {
+    todoId++;
+
     const todoText = document.getElementById("todo-input").value;
-    const todoItem = {text: todoText, status: "idle"};
+    const todoItem = {todoId: todoId++, text: todoText, status: "idle"};
     todoList.push(todoItem);
-    console.log(todoItem);
-
     localStorage.setItem(todoKey, JSON.stringify(todoList)); 
-    console.log(window.localStorage.getItem("todos"));
+    renderTodo()
+    console.log(todoList)
 }
 
+// function deleteTodo(event) {
+//     console.log(event);
+// }
 
-function deleteTodo(event) {
-    alert(event);
-}
+deleteBtn.addEventListener("click", (e)=>deleteTodo(e.target.parentNode));
